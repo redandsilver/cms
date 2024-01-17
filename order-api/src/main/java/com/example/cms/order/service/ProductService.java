d1c72f697191b947f10562e14aa95aedab25dd09
jrnecki <129943670+jrnecki@users.noreply.github.com>
2024-01-17 23:36:14 +0900

    feat : [008-PRODUCT_MODIFY] Product & ProductItem modify

--- order-api/src/main/java/com/example/cms/order/service/ProductService.java
+++ order-api/src/main/java/com/example/cms/order/service/ProductService.java
@@ -1,7 +1,12 @@
 package com.example.cms.order.service;
 
 import com.example.cms.order.domain.model.Product;
+import com.example.cms.order.domain.model.ProductItem;
 import com.example.cms.order.domain.product.AddProductForm;
+import com.example.cms.order.domain.product.UpdateProductForm;
+import com.example.cms.order.domain.product.UpdateProductItemForm;
+import com.example.cms.order.exception.CustomException;
+import com.example.cms.order.exception.ErrorCode;
 import com.example.cms.order.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,4 +22,22 @@ public class ProductService {
         return productRepository.save(Product.of(sellerId,form));
     }
 
+    @Transactional
+    public Product updateProduct(Long sellerId, UpdateProductForm form){
+        Product product = productRepository.findBySellerIdAndId(sellerId,form.getId())
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_PRODUCT));
+        product.setName(form.getName());
+        product.setDescription(form.getDescription());
+
+        for(UpdateProductItemForm itemForm : form.getItems()){
+            ProductItem item = product.getProductItems().stream().filter(
+                    pi -> pi.getId().equals(itemForm.getId()))
+                    .findFirst().orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_ITEM));
+            item.setName(itemForm.getName());
+            item.setPrice(itemForm.getPrice());
+            item.setCount(itemForm.getCount());
+
+        }
+        return product;
+    }
 }
